291704d28707e71b67ce7de452d11bba9170ceb3
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-06-20 19:49:40 -0600

    Renombre main.py a app.py dentro del folder

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from pjecz-hercules-api-key!")
+"""
+Programa principal: arrancar FastAPI con Uvicorn
 
+Equivale al comando:
+
+    uvicorn --host=127.0.0.1 --port 8000 --reload pjecz_hercules_api_key.app:app
+"""
+
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("pjecz_hercules_api_key.app:app", host="127.0.0.1", port=8000, reload=True)
